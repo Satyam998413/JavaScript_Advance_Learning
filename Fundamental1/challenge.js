f34fcf12d26e03a44e6e2636127b40531a800967
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-07-07 16:51:27 +0530

    Fundamental1
    first fundamental of javascript

---
+++ Fundamental1/challenge.js
@@ -0,0 +1,64 @@
+// // challenge 1
+// let john={
+//     mass:55,
+//     height:56
+// }
+// let mark={
+//     mass:50,
+//     height:34
+// }
+
+
+// johnBmi=john.mass/john.height**2;
+// markBmi=mark.mass/(mark.height*mark.height);
+// console.log(johnBmi,markBmi,johnBmi>markBmi);
+
+// challenge 2
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// // const massMark = 95;
+// // const heightMark = 1.88;
+// // const massJohn = 85;
+// // const heightJohn = 1.76;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+// }
+
+// challenge3
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy ');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy ');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+// BONUS 1
+// const scoreDolphins = (61546666697 + 5 + 80) / 3;
+// const scoreKoalas = (10458555432345619 + 95 + 50) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log('Dolphins win the trophy ');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log('Koalas win the trophy ');
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+//   console.log('Both win the trophy!');
+// } else {
+//   console.log('No oneone wins the trophy ');
+// }
+
+// challenge4
+const bill = 4800;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
